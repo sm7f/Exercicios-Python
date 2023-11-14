f4bc068826ae1fd4b7a5ce89236b84eb6ed0b750
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2023-11-13 22:24:57 -0300

    treino

--- 10.py
+++
@@ -1,6 +0,0 @@
-nota1 = float(input())
-nota2 = float(input())
-
-x = (nota1 * 7.5 + nota2 * 6.5) / 10.0
-
-print(x)
--- 3.py
+++ 3.py
@@ -5,13 +5,14 @@ num1 = 150
 num2 = 250
 
 
-a = num1 > num2
-b = num1 < num2
-c = num1 == num2 
-d = num1 != num2 
-d = num1 <= num2 
-e = num1 <= num2 and num1 <= num2
-f = num1 >= num2 or num1 >= num2
+a = num1 > num2 #maior que
+b = num1 < num2 #menor que
+c = num1 == num2 #igual a 
+d = num1 != num2 # diferente
+d = num1 <= num2 # menor ou igual
+e = num1 >= num2 # maior ou igual
+f = num1 <= num2 and num1 <= num2 #menor ou igual retorna verdadeiro se ambos tiver a mesma condição.
+g = num1 >= num2 or num1 >= num2  #maior ou igual retorna verdadeiro se a primeira condição for verdadeira.
 
 
 
